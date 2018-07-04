f5e827f87213e474fc5cd3923a972f0591ddbb96
Shartul Kumar <Shartul.Kumar.consultant@nielsen.com>
2018-07-04 18:57:13 +0530

    added "login data"

---
+++ utils/userData.js
@@ -0,0 +1,6 @@
+
+// store user data in maps for ease of use and readability...
+var UserData = function() {
+    this.testUser = {'username': 'shartul.kumar.consultant@nielsen.com','password': 'TNC$7648'};
+};
+module.exports = new UserData;
