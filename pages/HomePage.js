05688420ea73998f097337903cd3b5d6be91db2b
Shartul Kumar <Shartul.Kumar.consultant@nielsen.com>
2018-07-04 18:47:29 +0530

    added "new methods created in HomePage"

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,9 +1,23 @@
 //require('../elements/customlocators.js');
+var basePage = require('../pages/BasePage');
 
-var HomePage = function(){
+var HomePage = function() {
 
+    this.pageLoaded = this.and(
+        this.hasText($('title'))//Consumer Decision Tree
+    );
 
-    return require('./BasePage.js');
+    this.cards = function () {
+        var cards = element.all(by.css('span.tileHeader'));
+        return cards.getText();
+    };
 
+    this.searchFor = function(text) {
+        this.searchBox.sendKeys(text);
+        this.hitEnter();
+        browser.wait(this.isVisible(this.searchResultsPage), this.timeout.l);
+    };
 };
+HomePage.prototype = basePage; //extending base page
+
 module.exports = new HomePage();
