a6d7ea35cd7b4725b31376058a44c9d70b3c095f
Shartul Kumar <Shartul.Kumar.consultant@nielsen.com>
2018-08-09 22:49:35 +0530

    added "new node packages added"

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,6 +1,6 @@
 //require('../elements/customlocators.js');
 var basePage = require('../pages/BasePage');
-var JSONReader = require('../treeMetaData');
+var JSONReader = require('../treeMetaData.json');
 
 HomePage = function () {
 
@@ -12,27 +12,22 @@ HomePage = function () {
         var cards = element.all(by.css('span.tileHeader'));
         return cards.getText();
     };
-
-    this.clickatCard =  function(){
-        for(var j=0;j<JSONReader.result.length;j++) {
-            if (JSONReader.result[j].lockedByEmail = null) {
-                var elements = element.all(by.css('span.tileHeader'));
-                for(var k=0;j<elements.length;k++){
-                    if (elements[k].getText().isEqualTo(JSONReader.result[j].name)){
-                        browser.sleep(10000);
-                        elements.get(k).click();
-                        browser.sleep(10000);
-                        console.log("The locked card name is" + elements[k].getText());
-                    }
-                }
-            }
-
-        }
+    this.clickatUnlockCard= function (tileText) {
+        //Select all date elements and apply filter function
+        element.all(by.css('span.tileHeader')).filter(function (elem) {
+            return elem.getText().then(function (text) {
+                return text === tileText;
+            });
+        }).then(function (filteredElements) {
+            browser.sleep(20000);
+            browser.executeScript("arguments[0].scrollIntoView();",filteredElements[0]);
+            filteredElements[0].click();
+            browser.sleep(50000);
+        });
     };
-
     this.searchlockedCard =  function(text) {
         for (var j = 0; j < JSONReader.result.length; j++) {
-            if (JSONReader.result[j].lockedByEmail != null) {
+            if (JSONReader.result[j].lockedByEmail='null') {
                 var elements = element.all(by.css('span.tileHeader'));
                 for (var k = 0; j < elements.length; k++) {
                     if (JSONReader.result[j].name.isEqual(elements[k].getText())) {
@@ -48,12 +43,11 @@ HomePage = function () {
         }return lockedstatus;
 
     };
-
     this.searchFor = function (text) {
         this.searchBox.sendKeys(text);
         this.hitEnter();
         browser.wait(this.isVisible(this.searchResultsPage), this.timeout.l);
-    };
+        };
 };
 HomePage.prototype = basePage; //extending base page
 
