6e0587542f51ffa92de1cf9e73cd100d34f72566
Shartul Kumar <Shartul.Kumar.consultant@nielsen.com>
2018-08-09 22:46:01 +0530

    added "new code for CDT-4"

--- test_spec/CDTNavBar_spec.js
+++ test_spec/CDTNavBar_spec.js
@@ -5,39 +5,38 @@ var OR = require('../elements/OR.json');
 var logger = require('../utils/log.js');
 //var locator = require("../elements/uniqueLocator.js");
 var userData = require('../utils/userData');
-
+var JSONReader = require('../treeMetaData.json');
 
 
 describe("CDT Navigation Bar Test", function() {
-
     beforeEach(function () {
         browser.ignoreSynchronization = true;
     });
-
+    /*CDT-1: IE 11 popup and Chrome new tab navigation to CDT from answers dev portal site*/
     it("Validate navigation to CDT Home Page", function () {
         loginPage.to();
       //  expect(loginPage.getPageTitle()).toEqual('Nielsen Answers Login');
         loginPage.loginAs(userData.testUser);
         loginPage.selectCDT();
-        browser.sleep(5000);
+        browser.sleep(10000);
         browser.isInternetExplorer = function () {
             loginPage.loginAs(userData.testUser);
         };
         //expect(homePage.getPageTitle()).toEqual('Consumer Decision Tree');
     });
-
-   /* it("Validate the Card names on CDT Home Page", function () {
+    /*CDT-3: Validate tha Cards are displayed and count is verified */
+    it("Validate the Card names on CDT Home Page", function () {
         homePage.cards().then(function (text) {
             console.log("Total : " + text.length);
             logger.log('info', 'Card names are saved');
-            expect(text.length).toBe(8); //Jasmine test to check total card count
+            expect(text.length).toBe(text.count); //Jasmine test to check total card count
             logger.log('info', 'Card count is verified');
             browser.sleep(3000);
             console.log(text);
             logger.log('info', 'All card names are logged in the order');
         });
     });
-
+    /*CDT-3: Verify that Tree are visible in alphabetical order on Home Page*/
     it("Validate that Card names are sorted", function () {
         var sorted = [],
             unSorted = [];
@@ -53,9 +52,18 @@ describe("CDT Navigation Bar Test", function() {
             logger.log('info', 'Card names are sorted on CDT Home Page');
         });
     });
-*/
+    /*CDT-4: Check the unlocked card name from REST response and  then
+    use it to click and verify that card is locked */
     it("Validate that user can open the unlocked tree", function () {
-         homePage.clickatCard() ;
-         //console.log("The unlocked card is clicked");
+        for (var j = 0; j < JSONReader.result.length; j++) {
+            if (JSONReader.result[j].lockedByEmail === null) {
+            break;
+            }
+        } var cardname = JSONReader.result[j].name;
+          homePage.clickatCard(cardname);
+        console.log("The locked card name is" + cardname);
+        expect(editTreePage.pageLoaded()).toBeTruthy();
     });
+
 });
+           //
